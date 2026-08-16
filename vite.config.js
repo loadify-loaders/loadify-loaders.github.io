40f13d0f3c58649ec0109f303a9a93b71b12de8c
akh7il <akhilprsnl01@gmail.com>
2026-08-16 22:46:04 +0530

    UPDATE BASE PATH FOR GITHUB PAGE DEPLOYMENT

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/LOADIFY/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
