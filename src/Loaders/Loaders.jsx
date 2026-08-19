6fe3cc1e10b5da11e3fecd80ac7cee97d3c66e40
akh7il <akhilprsnl01@gmail.com>
2026-08-19 22:09:59 +0530

    ADD LOADER2

--- src/Loaders/Loaders.jsx
+++ src/Loaders/Loaders.jsx
@@ -1,4 +1,5 @@
-import Loader from "../Components/Loaders/Loader1/Loader"
+import Loader1 from '../Components/Loaders/Loader1/Loader'
+import Loader2 from '../Components/Loaders/Loader2/Loader'
 
 const LoadersData = [
     {
@@ -10,18 +11,11 @@ const LoadersData = [
 `,
         css:
             `
-:root {
-    --primary-color: #fff;
-    --width: 3.5rem;
-    --border-radius: 50%;
-    --border-width: .4rem;
-}
-
 .circle {
-    width: var(--width);
+    width: 3.5rem;
     aspect-ratio: 1;
-    border-radius: var(--border-radius);
-    border: var(--border-width) solid var(--primary-color);
+    border-radius: 50%;
+    border: .4rem solid #fff;
     border-bottom-color: transparent;
     border-left-color: transparent;
     animation: rotate 1s linear infinite;
@@ -34,7 +28,44 @@ const LoadersData = [
 }
     
 `,
-        component: <Loader />
+        component: <Loader1 />
+    },
+    {
+        id: 2,
+        category: 'rectangle',
+        react:
+            `
+<div className='loader'>
+    <div className='rectangle'></div>
+</div>
+
+`,
+        css:
+            `
+.loader {
+    perspective: 500px;
+}
+
+.rectangle {
+    width: 3.5rem;
+    aspect-ratio: 1;
+    background-color: #fff;
+    border-radius: .2rem;
+    animation: flip 1s linear infinite;
+}
+
+@keyframes flip {
+    50% {
+        transform: rotateY(180deg);
+    }
+
+    100% {
+        transform: rotateZ(180deg);
+    }
+}
+
+`,
+        component: <Loader2 />
     }
 ]
 
