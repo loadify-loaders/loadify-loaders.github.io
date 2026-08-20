2041227012bd4aa5a1b9397453d2d6d78e004c77
akh7il <akhilprsnl01@gmail.com>
2026-08-20 23:21:13 +0530

    ADD LOADER3

---
+++ src/Components/Loaders/Loader3/Loader.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from './Loader.module.css'
+
+export default function Loader() {
+    return (
+        <div className={styles['loader']}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    )
+}
--- src/Loaders/Loaders.jsx
+++ src/Loaders/Loaders.jsx
@@ -1,5 +1,6 @@
 import Loader1 from '../Components/Loaders/Loader1/Loader'
 import Loader2 from '../Components/Loaders/Loader2/Loader'
+import Loader3 from '../Components/Loaders/Loader3/Loader'
 
 const LoadersData = [
     {
@@ -12,7 +13,7 @@ const LoadersData = [
         css:
             `
 .circle {
-    width: 3.5rem;
+    width: 3rem;
     aspect-ratio: 1;
     border-radius: 50%;
     border: .4rem solid #fff;
@@ -66,6 +67,105 @@ const LoadersData = [
 
 `,
         component: <Loader2 />
+    },
+    {
+        id: 3,
+        category: 'circle',
+        react:
+            `
+
+<div className='loader'>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+    <span></span>
+</div>
+
+`,  
+        css: 
+        `
+
+.loader {
+    width: 3rem;
+    aspect-ratio: 1;
+    border-radius: 50%;
+    position: relative;
+    animation: rotate 2s linear infinite;
+}
+
+.loader span {
+    --i: 0;
+    position: absolute;
+    width: .3rem;
+    height: 25%;
+    background-color: #fff;
+    left: 50%;
+    top: 0;
+    transform: rotate(calc(var(--i) * 30deg)) translate(-50%, 0);
+    transform-origin: 0 1.5rem;
+    border-radius: .3rem;
+}
+
+.loader span:nth-child(2) {
+    --i: 1
+}
+
+.loader span:nth-child(3) {
+    --i: 2
+}
+
+.loader span:nth-child(4) {
+    --i: 3
+}
+
+.loader span:nth-child(5) {
+    --i: 4
+}
+
+.loader span:nth-child(6) {
+    --i: 5
+}
+
+.loader span:nth-child(7) {
+    --i: 6
+}
+
+.loader span:nth-child(8) {
+    --i: 7
+}
+
+.loader span:nth-child(9) {
+    --i: 8
+}
+
+.loader span:nth-child(10) {
+    --i: 9
+}
+
+.loader span:nth-child(11) {
+    --i: 10
+}
+
+.loader span:nth-child(12) {
+    --i: 11
+}
+
+@keyframes rotate {
+    to {
+        transform: rotate(360deg);
+    }
+}
+
+        `,
+        component: <Loader3 />
     }
 ]
 
