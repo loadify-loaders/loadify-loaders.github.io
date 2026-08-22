7feefaa237512febbb479905aca149530f54b161
akh7il <akhilprsnl01@gmail.com>
2026-08-23 02:46:23 +0530

    ADD LOADER5

---
+++ src/Components/Loaders/Loader5/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Loader.module.css'
+
+export default function Loader() {
+    return (
+        <div className={styles['loader']}>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    )
+}
